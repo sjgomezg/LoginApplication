27a053b0220bbdbff270ca0d339af39591a2cce3
moroc <moroc@DESKTOP-I1525J2>
2019-10-04 21:57:20 -0500

    Lab2realizado

--- src/Control/ValidarLogin.java
+++ src/Control/ValidarLogin.java
@@ -14,42 +14,39 @@ import Frontera.FramePrincipal;
  * @author usuario
  */
 public class ValidarLogin {
-    
+
     private Sistema sistema = FramePrincipal.sistema;
-    
-    public ValidarLogin(){
-        
+
+    public ValidarLogin() {
+
     }
-    
-    public String verificarLogin(Usuario usuario){
-        if(!verificarLongitudNombre(usuario.getNombre())){
-            return("Longitud de nombre incorrecta");
+
+    public String verificarLogin(Usuario usuario) {
+        
+        if(!verificarLongitudNombre(usuario.getNombre())) {
+                return ("Longitud de nombre incorrecta");
         }
-        if(!verificarLongitudPassword(usuario.getPassword())){
-            return("Longitud de contraseña incorrecta");
-        }
-        if(!verificarPassword(usuario.getPassword())){
-            return("Longitud de contraseña incorrecta");
-        }
-        for(Usuario u: sistema.getUsuarios()){
-            if(u.getNombre().equals(usuario.getNombre()) &&
-               u.getPassword().equals(usuario.getPassword())){
-            return("Bienvenido");
-        }
+        
+        if (!verificarPassword(usuario.getPassword())) {
+            return ("Longitud de contraseña incorrecta");
+        } 
+        
+        for (Usuario u : sistema.getUsuarios()) {
+            
+            if (u.getNombre().equals(usuario.getNombre())
+                    && u.getPassword().equals(usuario.getPassword())) {
+                return ("Bienvenido");
+            }
         }
         return ("Datos Incorrectos");
     }
     
-    
-    public boolean verificarLongitudNombre(String nombre){
-        return (nombre.length()>1 && nombre.length()<= 6);
+    public boolean verificarLongitudNombre(String password) {
+        return (password.length() > 1 && password.length() <= 6);
     }
-    
-    public boolean verificarLongitudPassword(String password){
-        return(password.length()>=3 && password.length()< 6);
-    }
-    public boolean verificarPassword(String password){
-        return(password.length()>=3 && password.length()< 6);
-    }
-}
 
+    public boolean verificarPassword(String password) {
+        return(password.length()>=3 && password.length() < 6);
+    }
+    
+}
